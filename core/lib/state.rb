0bd5381155b96e251f2a83029ae9b3b78c076480
IceDragon200 <mistdragon100@gmail.com>
2014-04-07 13:12:37 -0500

    Updated state with comments and init method

--- core/lib/state.rb
+++ core/lib/state.rb
@@ -1,30 +1,74 @@
+###
+# State
+###
 class State
 
+  ###
+  # @type [Array<State>]
+  ###
   @states = []
 
+  ###
+  # @type [???]
+  ###
   attr_accessor :engine
 
+  ###
+  # @param [???] engine
+  ###
   def initialize(engine=nil)
     @engine = engine
     @ticks = 0
+    init
   end
 
+  ###
+  # Init
+  # @return [void]
+  ###
+  def init
+    #
+  end
+
+  ###
+  # Per frame update function, called by moon
+  # @return [void]
+  ###
   def update
     @ticks += 1
   end
 
+  ###
+  # Per frame render function, called by moon
+  # @return [void]
+  ###
   def render
     #
   end
 
+  ###
+  # Callback function, called by the State class, when the state is
+  # added to the stack
+  # @return [void]
+  ###
   def on_push
     #
   end
 
+  ###
+  # Callback function, called by the State class, when the state is
+  # removed from the stack
+  # @return [void]
+  ###
   def on_pop
     #
   end
 
+  ###
+  # Swap the current state with (state)
+  # @param [State] state
+  # @return [State] old_state
+  ###
   def self.change(state)
     old_state = @states[-1]
     old_state.on_pop
@@ -33,12 +77,21 @@ class State
     old_state
   end
 
+  ###
+  # Add a new state to the stack
+  # @param [State] state
+  # @return [State] state
+  ###
   def self.push(state)
     @states.push state
     state.on_push
     state
   end
 
+  ###
+  # Remove the current state from the stack
+  # @return [State] old_state
+  ###
   def self.pop
     old_state = @states.pop
     old_state.on_pop
--- scripts/lib/es/states/splash.rb
+++ scripts/lib/es/states/splash.rb
@@ -2,6 +2,11 @@ module ES
   module States
     class Splash < State
 
+      def init
+        @bitmapfont = BitmapFont.new("media/bmpfont/font_cga8_white.png")
+        @bitmapfont.string = "Splash"
+      end
+
       def update
         #
         super
@@ -9,6 +14,9 @@ module ES
 
       def render
         #
+        x = (Moon::Screen.width - @bitmapfont.width) / 2
+        y = (Moon::Screen.height - @bitmapfont.height) / 2
+        @bitmapfont.render(x, y, 0)
         super
       end
 
