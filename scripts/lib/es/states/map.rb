dc820a1ca097d6c443634a063d5c055bcbf93641
IceDragon200 <mistdragon100@gmail.com>
2014-04-08 01:03:24 -0500

    Added ES::States::Map stub

---
+++ scripts/lib/es/states/map.rb
@@ -0,0 +1,19 @@
+module ES
+  module States
+    class Map < State
+
+      def init
+        super
+      end
+
+      def update
+        super
+      end
+
+      def render
+        super
+      end
+
+    end
+  end
+end
