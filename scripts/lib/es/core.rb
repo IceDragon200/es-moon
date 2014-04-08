df0f3e62064e5105eea8bbe4d1740bb79d403167
IceDragon200 <mistdragon100@gmail.com>
2014-04-07 20:35:19 -0500

    Added Countdown class

--- scripts/lib/es/core.rb
+++ scripts/lib/es/core.rb
@@ -1,2 +1,3 @@
+require 'scripts/lib/es/core/countdown'
 require 'scripts/lib/es/core/bitmap_font'
 require 'scripts/lib/es/core/render_layer'
---
+++ scripts/lib/es/core/countdown.rb
@@ -0,0 +1,19 @@
+class Countdown
+
+  def initialize(n)
+    @ticks = @ticks_max = n.to_i
+  end
+
+  def done?
+    @ticks < 0
+  end
+
+  def update
+    @ticks -= 1 if @ticks > 0
+  end
+
+  def rate
+    @ticks.to_f / @ticks_max
+  end
+
+end
