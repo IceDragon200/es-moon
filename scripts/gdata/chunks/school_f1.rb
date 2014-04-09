e706ccd54937c1991ddea146388f3a3c9be81054
IceDragon200 <mistdragon100@gmail.com>
2014-04-08 22:34:07 -0500

    Some test data for Map

--- scripts/gdata/chunks/school_f1.rb
+++ scripts/gdata/chunks/school_f1.rb
@@ -1,6 +1,12 @@
 ES::Database.create :chunk do |chunk|
+
   chunk.name = "school_f1/baron.room"
-  chunk.data = DataMatrix.new(10, 10, 1) do |dm|
-    dm
+  chunk.data = DataMatrix.new(10, 10, 2) do |dm|
+    dm.fill(-1) # clear
+    dm[0, 0, 0] = 20
+    dm[9, 0, 0] = 22
+    dm[0, 9, 0] = 40
+    dm[9, 9, 0] = 42
   end
+
 end
--- scripts/lib/es/states/map.rb
+++ scripts/lib/es/states/map.rb
@@ -4,6 +4,13 @@ module ES
 
       def init
         super
+        @camera = Camera2.new
+        @tilemap = Tilemap.new do |tilemap|
+          filename = "tileset_16x16_Jerom_CC-BY-SA-3.0_8_blue.png"
+          chunk = ES::Database.find :chunk, name: "school_f1/baron.room"
+          tilemap.tileset = Cache.tileset(filename, 16, 16)
+          tilemap.data = chunk.data
+        end
       end
 
       def update
@@ -11,6 +18,7 @@ module ES
       end
 
       def render
+        @tilemap.render 0, 0, 0
         super
       end
 
