c626734cfab3a0bbfc69098ae8c71f5f1d2cc0e4
IceDragon200 <mistdragon100@gmail.com>
2014-04-08 23:04:13 -0500

    Map and Camera position

--- scripts/lib/es/states/map.rb
+++ scripts/lib/es/states/map.rb
@@ -5,6 +5,7 @@ module ES
       def init
         super
         @camera = Camera2.new
+        @map_pos = Vector3.new 0, 0, 0
         @tilemap = Tilemap.new do |tilemap|
           filename = "tileset_16x16_Jerom_CC-BY-SA-3.0_8_blue.png"
           chunk = ES::Database.find :chunk, name: "school_f1/baron.room"
@@ -18,7 +19,7 @@ module ES
       end
 
       def render
-        @tilemap.render 0, 0, 0
+        @tilemap.render(*(@map_pos - @camera.position.xyz))
         super
       end
 
