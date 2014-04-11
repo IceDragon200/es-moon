de0ea08eebb9083ce4aeb73cae355515066c049d
IceDragon200 <mistdragon100@gmail.com>
2014-04-11 00:10:27 -0500

    Added ES::UI::PositionMonitor class

--- scripts/lib/es/ui.rb
+++ scripts/lib/es/ui.rb
@@ -1,1 +1,2 @@
+require 'scripts/lib/es/ui/position_monitor'
 require 'scripts/lib/es/ui/title_menu'
---
+++ scripts/lib/es/ui/position_monitor.rb
@@ -0,0 +1,32 @@
+module ES
+  module UI
+    class PositionMonitor < BitmapFont
+
+      def initialize
+        super "font_cga8_white.png"
+        @obj = nil
+      end
+
+      def update
+        if @obj
+          o = @obj
+          o = @obj.position if @obj_uses_internal_position
+          ##
+          # https://github.com/mruby/mruby/issues/2042
+          #set_string("x: #{o.x.round(2)}, y: #{o.y.round(2)}")
+          #set_string("x: #{o.x.to_i}, y: #{o.y.to_i}")
+          # patched
+          set_string("x: #{o.x.round(2)}, y: #{o.y.round(2)}")
+        else
+          set_string("x: -.-, y: -.-")
+        end
+      end
+
+      def set_obj(obj, obj_uses_internal_position=false)
+        @obj = obj
+        @obj_uses_internal_position = obj_uses_internal_position
+      end
+
+    end
+  end
+end
