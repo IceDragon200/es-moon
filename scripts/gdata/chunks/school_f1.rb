5a2c3e5f1af53f6fce655e5cb0717201d8397e2e
IceDragon200 <mistdragon100@gmail.com>
2014-04-10 21:15:04 -0500

    Now abusing new PaintMap features, I finished baron's room!

--- scripts/gdata/chunks/school_f1.rb
+++ scripts/gdata/chunks/school_f1.rb
@@ -1,16 +1,25 @@
 ES::Database.create :chunk do |chunk|
 
   chunk.name = "school_f1/baron.room"
-  chunk.data = DataMatrix.new(10, 10, 2) do |dm|
+  chunk.data = DataMatrix.new(8, 6, 2) do |dm| dm.clear(-1)
     painter = ES::Helper::PaintMap.new(dm)
     painter.save do |pnt|
-      # contract
-      pnt.clear
-      pnt.set_layer(0)
-      pnt.rect.contract(1)
-      pnt.set_value(0).stroke
-      pnt.set_value(12).fill
-      pnt.set_layer(1).clear
+      ## Ground layer                               #
+      pnt.set_layer( 0).clear                       # ground layer
+      pnt.rect.contract( 1)                         # inner
+      pnt.set_value( 0).stroke                      # walls
+      pnt.set_value( 12).fill                       # fill floor
+      pnt.set_rect(3, 2, 3, 3).set_value( 53).fill  # fill rug
+      pnt.reset_rect                                # restore rect
+      pnt.set_pos( 2,  0).set_value( 1).point       # wall lamp
+      pnt.move( 3,  0).set_value( 1).point          # wall lamp
+      ## Detail layer                               #
+      pnt.set_pos( 0,  0).set_layer( 1).clear       # goto second layer
+      pnt.move( 1,  1).set_value( 135).point        # bed
+      pnt.move( 3,  0).set_value( 141).point        # bookshelf
+      pnt.move( 0,  2).set_value( 140).point        # table
+      pnt.move(-1,  0).set_value( 138).point        # left chair
+      pnt.move( 2,  0).set_value( 137).point        # right chair
     end
     painter.render
   end
