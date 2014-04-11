49a840210224298464933f6797839da26276acd6
IceDragon200 <mistdragon100@gmail.com>
2014-04-11 00:14:52 -0500

    Added some pointless version text watermarking madness.

--- scripts/lib/es/states.rb
+++ scripts/lib/es/states.rb
@@ -1,3 +1,26 @@
+class State
+
+  alias :init_wo_version_bmpfont :init
+  def init
+    init_wo_version_bmpfont
+    @beta_layer = RenderLayer.new
+    bmpfont = BitmapFont.new("font_cga8_white.png")
+    bmpfont.set_string("moon-0.1.0")
+    bmpfont.position.set(Moon::Screen.width - bmpfont.width, 0, 0)
+    @beta_layer.add(bmpfont)
+    bmpfont2 = BitmapFont.new("font_cga8_white.png")
+    bmpfont2.set_string("es-moon-#{ES::Version::STRING}")
+    @beta_layer.add(bmpfont2)
+    @beta_layer.position.set(Moon::Screen.width - @beta_layer.width,
+                             Moon::Screen.height - @beta_layer.height, 0)
+  end
+
+  def render
+    @beta_layer.render
+  end
+
+end
+
 module ES
   module States
   end
