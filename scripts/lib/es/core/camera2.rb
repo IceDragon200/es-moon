17a6ca5bb11003ccb53b6dfd4864f3d2e99272ea
IceDragon200 <mistdragon100@gmail.com>
2014-04-11 18:34:19 -0500

    Added Camera2#follow

--- scripts/lib/es/core/camera2.rb
+++ scripts/lib/es/core/camera2.rb
@@ -1,9 +1,32 @@
 class Camera2
 
   attr_accessor :position
+  attr_reader :obj
 
   def initialize
     @position = Vector2.new 0, 0
+    @dest_position = Vector2.new 0, 0
+    @easer = Easer.new
+    @countdown = Countdown.new(15)
+    @view = Moon::Rect.new(-Moon::Screen.width/2, -Moon::Screen.height/2,
+                            Moon::Screen.width/2,  Moon::Screen.height/2)
+  end
+
+  def follow(obj)
+    @obj = obj
+    @dest_position
+  end
+
+  def update
+    if @obj
+      a = @obj.position.xy * 32 + @view.xy
+      if @dest_position != a
+        @dest_position.set(*a)
+        @countdown.reset
+      end
+      @position = @easer.ease(@position, @dest_position, @countdown.rate)
+      @countdown.update
+    end
   end
 
 end
