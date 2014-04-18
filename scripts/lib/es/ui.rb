6612a4c662b64d0948580f10a67f23d6c7ac72ec
IceDragon200 <mistdragon100@gmail.com>
2014-04-18 18:35:06 -0500

    Added UI::TilePanel

--- scripts/lib/es/ui.rb
+++ scripts/lib/es/ui.rb
@@ -1,2 +1,3 @@
 require 'scripts/lib/es/ui/position_monitor'
 require 'scripts/lib/es/ui/title_menu'
+require 'scripts/lib/es/ui/tile_panel'
---
+++ scripts/lib/es/ui/tile_panel.rb
@@ -0,0 +1,11 @@
+module ES
+  module UI
+    class TilePanel < RenderContainer
+
+      def render(x, y, z)
+
+      end
+
+    end
+  end
+end
