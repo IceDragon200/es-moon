bc01b1447e82b9953435c8d7a2a997d46395fc19
IceDragon200 <mistdragon100@gmail.com>
2014-04-18 19:44:33 -0500

    chunk data should default to -1

--- scripts/data/chunks/school_f1.rb
+++ scripts/data/chunks/school_f1.rb
@@ -3,6 +3,8 @@ ES::Database.create :chunk do |chunk|
   chunk.name = "school_f1/room.baron"
 
   chunk.data = DataMatrix.new(8, 6, 2) do |dm| dm.clear(-1)
+    dm.default = -1
+
     pnt = ES::Helper::PaintMap.new(dm)
     ### Ground layer                                           #
     pnt.layer = 0
@@ -52,6 +54,8 @@ ES::Database.create :chunk do |chunk|
   chunk.name = "school_f1/hallway"
 
   chunk.data = DataMatrix.new(7, 12, 2) do |dm| dm.clear(-1)
+    dm.default = -1
+
     pnt = ES::Helper::PaintMap.new(dm)
     pnt.layer = 0
     pnt.clear                                                  #
