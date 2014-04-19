6ee5974162e8a80eb32fe67a3b95e1332627ff8a
IceDragon200 <mistdragon100@gmail.com>
2014-04-19 01:51:37 -0500

    Fixed reduce not expanding args correctly

--- scripts/lib/core_ext/moon-vector2.rb
+++ scripts/lib/core_ext/moon-vector2.rb
@@ -1,7 +1,7 @@
 class Vector2
 
   def reduce(*args)
-    rx, ry = Vector2.obj_to_vec2_a(args.size > 1 ? args : args.first)
+    rx, ry = *Vector2.obj_to_vec2_a(args.size > 1 ? args : args.first)
     Vector2.new (x / rx).floor * rx, (y / ry).floor * ry
   end
 
--- scripts/lib/core_ext/moon-vector3.rb
+++ scripts/lib/core_ext/moon-vector3.rb
@@ -1,7 +1,7 @@
 class Vector3
 
   def reduce(*args)
-    rx, ry, rz = Vector3.obj_to_vec3_a(args.size > 1 ? args : args.first)
+    rx, ry, rz = *Vector3.obj_to_vec3_a(args.size > 1 ? args : args.first)
     Vector3.new (x / rx).floor * rx, (y / ry).floor * ry, (z / rz).floor * rz
   end
 
