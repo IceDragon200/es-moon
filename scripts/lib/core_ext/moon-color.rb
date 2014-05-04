511aa2c0b28a07359c023c353b3b85c967e12f71
IceDragon200 <mistdragon100@gmail.com>
2014-05-04 14:28:51 -0500

    Added Moon::Color coerce, and removed ::extract

--- scripts/lib/core_ext/moon-color.rb
+++ scripts/lib/core_ext/moon-color.rb
@@ -1,12 +1,16 @@
 module Moon
   class Color
 
+    def coerce(other)
+      return self, other
+    end
+
     def -@
       Color.new 1 - r, 1 - g, 1 - b, a
     end
 
     def +@
-      Color.new r, g, b, a
+      Color.new r.abs, g.abs, b.abs, a.abs
     end
 
     def +(other)
@@ -29,23 +33,7 @@ module Moon
       Color.new r / ar, g / ag, b / ab, a * aa
     end
 
-    class << self
-
-      def extract(other)
-        case other
-        when Color then return *other
-        when Array then
-          r, g, b, a = *other
-          a ||= 1.0
-          return r, g, b, a
-        when Numeric then return other, other, other, 1.0
-        else
-          raise TypeError,
-                "wrong argument type #{obj.class} (expected Color, Array or Numeric)"
-        end
-      end unless method_defined? :extract
-
-    end
+    alias :abs :+@
 
   end
 end
