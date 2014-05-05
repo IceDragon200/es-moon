32b3a5e07ac48f31727f5c4844d33f3ee6472e02
IceDragon200 <mistdragon100@gmail.com>
2014-05-05 15:58:34 -0500

    DataModel Chunk and Map save_file

--- scripts/lib/es/data_model/chunk.rb
+++ scripts/lib/es/data_model/chunk.rb
@@ -6,6 +6,19 @@ module ES
       field :flags,    type: DataMatrix, allow_nil: true, default: nil
       field :passages, type: Table,      allow_nil: true, default: nil
 
+      def save_file
+        path = ("data/chunks/" + name).split("/")
+        filename = path.pop
+        pathname = "#{path.join("/")}/#{filename}.yml"
+        path.size.times do |i|
+          pth = path[0, i+1].join("/")
+          Dir.mkdir pth unless Dir.exist? pth
+        end
+        File.open pathname, "w" do |file|
+          file.write YAML.dump(export)
+        end
+      end
+
     end
   end
 end
--- scripts/lib/es/data_model/map.rb
+++ scripts/lib/es/data_model/map.rb
@@ -7,6 +7,19 @@ module ES
       field :chunks,         type: [Hash],    default: proc {[]}
       field :chunk_position, type: [Vector3], default: proc {[]}
 
+      def save_file
+        path = ("data/maps/" + name).split("/")
+        filename = path.pop
+        pathname = "#{path.join("/")}/#{filename}.yml"
+        path.size.times do |i|
+          pth = path[0, i+1].join("/")
+          Dir.mkdir pth unless Dir.exist? pth
+        end
+        File.open pathname, "w" do |file|
+          file.write YAML.dump(export)
+        end
+      end
+
     end
   end
 end
