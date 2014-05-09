b6ba3290f3b6d67673dd9bc440074460d332f45c
IceDragon200 <mistdragon100@gmail.com>
2014-05-09 13:31:02 -0500

    Death to brackets!?

--- scripts/load.rb
+++ scripts/load.rb
@@ -6,6 +6,6 @@ require 'scripts/config'
 require 'scripts/lib'
 require 'scripts/data'
 
-State.push(ES::States::Shutdown)
-State.push(ES::States::Title)
-State.push(ES::States::Splash)
+State.push ES::States::Shutdown
+State.push ES::States::Title
+State.push ES::States::Splash
