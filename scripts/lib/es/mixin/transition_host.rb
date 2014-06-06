e6572ef9a4fc676dce46344477916a3dc328e56b
IceDragon200 <mistdragon100@gmail.com>
2014-06-06 18:08:09 -0500

    The Transition update!

--- scripts/lib/es/mixin/transition_host.rb
+++ scripts/lib/es/mixin/transition_host.rb
@@ -1,9 +1,35 @@
 module TransitionHost
 
+  ###
+  # @param [] *args
+  # @return [Transition]
+  ###
   def add_transition(*args, &block)
-    (@transitions ||= []).push Transition.new(*args, &block)
+    transition = Transition.new(*args, &block)
+    (@transitions ||= []).push transition
+    transition
   end
 
+  ###
+  # @param [Transition] transition
+  # @return [Void]
+  ###
+  def remove_transition(transition)
+    return unless @transitions
+    @transitions.delete transition
+  end
+
+  ###
+  # @param [Array<Transition>] transitions
+  # @return [Void]
+  ###
+  def remove_transitions(transitions)
+    @transitions -= transitions
+  end
+
+  ###
+  # @param [Float] delta
+  ###
   def update_transitions(delta)
     return unless @transitions
     return if @transitions.empty?
@@ -12,7 +38,18 @@ module TransitionHost
       transition.update delta
       dead << transition if transition.done?
     end
-    @transitions -= dead
+    unless dead.empty?
+      remove_transitions dead
+    end
+  end
+
+  ###
+  # Force all transitions to finish.
+  # @return [Void]
+  ###
+  def finish_transitions
+    return unless @transitions
+    @transitions.each(&:finish)
   end
 
 end
--- scripts/lib/es/mixin/transitionable.rb
+++ scripts/lib/es/mixin/transitionable.rb
@@ -1,5 +1,6 @@
 module Transitionable
 
+  ### mixins
   include TransitionHost
 
   ###
@@ -9,7 +10,7 @@ module Transitionable
   # @param [Object] value  target value
   # @param [Numeric] duration  in seconds
   ###
-  def transition(attribute, value, duration=0.15)
+  def transition(attribute, value, duration=0.20)
     attribute = attribute.to_s
     rolling = attribute.split(".")
     if rolling.size > 1
@@ -23,6 +24,37 @@ module Transitionable
       setter = "#{attribute}="
       add_transition(src, value, duration) { |v| send(setter, v) }
     end
+  end
+
+  ###
+  # Add a keyed transition
+  #   A keyed transition will only have 1 active transition per attribute
+  #   However multiple transition can still operate on the attribute if
+  #   they where not keyed.
+  # @param [String] attribute
+  #   @example "color"
+  #   @example "position.x"
+  # @param [Object] value  target value
+  # @param [Numeric] duration  in seconds
+  ###
+  def key_transition(attribute, value, duration=0.15)
+    @key_transition ||= {}
+    remove_transition(@key_transition[attribute]) if @key_transition.key?(attribute)
+    t = transition(attribute, value, duration)
+    t.key = attribute
+    @key_transition[attribute] = t
+    t
+  end
+
+  ###
+  # @param [Array<Transitions>] transitions
+  # @return [Void]
+  ###
+  def remove_transitions(transitions)
+    if @key_transition
+      transitions.each { |t| @key_transition.delete(t.key) if t.key }
+    end
+    super transitions
   end
 
 end
