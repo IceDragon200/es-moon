b8081e513c03ac41bab4a31f15a7f1e0cfa3da61
IceDragon200 <mistdragon100@gmail.com>
2014-06-15 15:14:59 -0500

    Updated Cache to use new Moon::CacheBase

--- scripts/lib/cache.rb
+++ scripts/lib/cache.rb
@@ -1,71 +1,82 @@
-module Cache
+module ES
+  class << self
+    attr_accessor :cache
+  end
+  class Cache < CacheBase
 
-  branch :palette do
-    lambda do |*args|
-      YAML.load(File.read("data/palette.yml")).each_with_object({}) do |a, r|
-        k, v = *a
-        r[k] = Vector4[v]
+    branch :palette do
+      lambda do |*args|
+        YAML.load(File.read("data/palette.yml")).each_with_object({}) do |a, r|
+          k, v = *a
+          r[k] = Vector4[v]
+        end
       end
     end
-  end
 
-  branch :charmap do
-    lambda do |filename, *args|
-      YAML.load(File.read("data/charmap/#{filename}"))
-    end
-  end
-
-  branch :block do
-    lambda do |filename, *args|
-      Moon::Spritesheet.new("media/blocks/" + filename, *args)
-    end
-  end
-
-  branch :icon do
-    lambda do |filename, *args|
-      Moon::Sprite.new("media/icons_64x64/" + filename)
-    end
-  end
-
-  branch :bmpfont do
-    lambda do |filename, *args|
-      Moon::Spritesheet.new("media/bmpfont/" + filename, *args)
-    end
-  end
-
-  branch :tileset do
-    lambda do |filename, *args|
-      Moon::Spritesheet.new("media/tilesets/" + filename, *args)
-    end
-  end
-
-  branch :font do
-
-    hsh = {}
-
-    loader = lambda do |filename|
-      ->(size) { Moon::Font.new(filename, size) }
+    branch :charmap do
+      lambda do |filename, *args|
+        YAML.load(File.read("data/charmap/#{filename}"))
+      end
     end
 
-    hsh["awesome"] = loader.("resources/fonts/fontawesome-webfont.ttf")
+    branch :block do
+      lambda do |filename, *args|
+        Moon::Spritesheet.new("media/blocks/" + filename, *args)
+      end
+    end
 
-    hsh["foundation"] = loader.("resources/fonts/general_foundicons.ttf")
-    hsh["foundation_enclosed"] = loader.("resources/fonts/general_enclosed_foundicons.ttf")
+    branch :icon do
+      lambda do |filename, *args|
+        Moon::Sprite.new("media/icons_64x64/" + filename)
+      end
+    end
 
-    hsh["uni0553"] = loader.("resources/fonts/uni0553/uni0553-webfont.ttf")
-    hsh["uni0554"] = loader.("resources/fonts/uni0554/uni0554-webfont.ttf")
-    hsh["uni0563"] = loader.("resources/fonts/uni0563/uni0563-webfont.ttf")
-    hsh["uni0564"] = loader.("resources/fonts/uni0564/uni0564-webfont.ttf")
+    branch :bmpfont do
+      lambda do |filename, *args|
+        Moon::Spritesheet.new("media/bmpfont/" + filename, *args)
+      end
+    end
 
-    hsh["ipaexg"] = loader.("resources/fonts/ipaexg00201/ipaexg.ttf")
+    branch :tileset do
+      lambda do |filename, *args|
+        Moon::Spritesheet.new("media/tilesets/" + filename, *args)
+      end
+    end
 
-    hsh["vera"] = loader.("resources/fonts/vera/Vera.ttf")
-    hsh["vera_mono"] = loader.("resources/fonts/vera/VeraMono.ttf")
-    hsh["vera_mono_bold_italic"] = loader.("resources/fonts/vera/VeraMoBI.ttf")
-    hsh["vera_mono_bold"] = loader.("resources/fonts/vera/VeraMoBd.ttf")
-    hsh["vera_mono_italic"] = loader.("resources/fonts/vera/VeraMoIt.ttf")
+    branch :system do
+      lambda do |filename, *args|
+        Moon::Sprite.new "media/system/" + filename
+      end
+    end
 
-    hsh
+    branch :font do
+
+      hsh = {}
+
+      loader = lambda do |filename|
+        ->(size) { Moon::Font.new(filename, size) }
+      end
+
+      hsh["awesome"] = loader.("resources/fonts/fontawesome-webfont.ttf")
+
+      hsh["foundation"] = loader.("resources/fonts/general_foundicons.ttf")
+      hsh["foundation_enclosed"] = loader.("resources/fonts/general_enclosed_foundicons.ttf")
+
+      hsh["uni0553"] = loader.("resources/fonts/uni0553/uni0553-webfont.ttf")
+      hsh["uni0554"] = loader.("resources/fonts/uni0554/uni0554-webfont.ttf")
+      hsh["uni0563"] = loader.("resources/fonts/uni0563/uni0563-webfont.ttf")
+      hsh["uni0564"] = loader.("resources/fonts/uni0564/uni0564-webfont.ttf")
+
+      hsh["ipaexg"] = loader.("resources/fonts/ipaexg00201/ipaexg.ttf")
+
+      hsh["vera"] = loader.("resources/fonts/vera/Vera.ttf")
+      hsh["vera_mono"] = loader.("resources/fonts/vera/VeraMono.ttf")
+      hsh["vera_mono_bold_italic"] = loader.("resources/fonts/vera/VeraMoBI.ttf")
+      hsh["vera_mono_bold"] = loader.("resources/fonts/vera/VeraMoBd.ttf")
+      hsh["vera_mono_italic"] = loader.("resources/fonts/vera/VeraMoIt.ttf")
+
+      hsh
+    end
+
   end
-
 end
