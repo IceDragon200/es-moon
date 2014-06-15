2617e55bd0abc97e189fc51956d1564a4df4e9d3
IceDragon200 <mistdragon100@gmail.com>
2014-06-15 15:15:51 -0500

    Added queryable to mixins

--- scripts/lib/es/mixin.rb
+++ scripts/lib/es/mixin.rb
@@ -1,6 +1,7 @@
 require 'scripts/lib/es/mixin/activatable'
 require 'scripts/lib/es/mixin/interval_host'
-require 'scripts/lib/es/mixin/transition_host'
+require 'scripts/lib/es/mixin/queryable'
 require 'scripts/lib/es/mixin/task_host'
+require 'scripts/lib/es/mixin/transition_host'
 require 'scripts/lib/es/mixin/transitionable'
 require 'scripts/lib/es/mixin/visibility'
