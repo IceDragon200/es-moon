d47a83b6903de90c75d0b9cdfaeb27f1110f99e9
IceDragon200 <mistdragon100@gmail.com>
2014-06-22 00:22:22 -0500

    Added moon logo to splash screen

--- scripts/lib/es/states/splash.rb
+++ scripts/lib/es/states/splash.rb
@@ -6,6 +6,9 @@ module ES
         super
         font = ES.cache.font "uni0553", 16
         @text = Text.new "Earthen : Smiths #{ES::Version::STRING}", font
+        @moon_logo = Moon::Sprite.new("resources/splash/moon-logo.png")
+        @moon_logo.ox = @moon_logo.width / 2
+        @moon_logo.oy = @moon_logo.height / 2
         add_task 2 do
           State.pop
         end
@@ -17,6 +20,9 @@ module ES
       end
 
       def render
+        x = (Moon::Screen.width - @moon_logo.width) / 2
+        y = (Moon::Screen.height - @moon_logo.height) / 2
+        @moon_logo.render x, y, 0
         x = (Moon::Screen.width - @text.width) / 2
         y = (Moon::Screen.height - @text.height) / 2
         @text.render(x, y, 0)
