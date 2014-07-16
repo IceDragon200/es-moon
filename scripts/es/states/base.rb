d0153c747501ca2e9c92bb1456ec59016b61db73
IceDragon200 <mistdragon100@gmail.com>
2014-07-15 19:29:15 -0500

    Removed TaskHost and IntervalHost

--- scripts/es/states/base.rb
+++ scripts/es/states/base.rb
@@ -4,14 +4,10 @@ module ES
   module States
     class Base < State
 
-      include TaskHost
-      include IntervalHost
       include TransitionHost
 
       def update(delta)
         super delta
-        update_tasks delta
-        update_intervals delta
         update_transitions delta
       end
 
