c35991997cc8ac2a48cb5dd6e903596878187fa2
IceDragon200 <mistdragon100@gmail.com>
2014-07-17 01:01:31 -0500

    Added Rect#cx= and Rect#cy=

--- scripts/core_ext/moon-rect.rb
+++ scripts/core_ext/moon-rect.rb
@@ -5,5 +5,13 @@ module Moon
       cy = cy.to_i
       Rect.new x + cx, y + cy, width - cx * 2, height - cy * 2
     end
+
+    def cx=(cx)
+      self.x = cx - width / 2
+    end
+
+    def cy=(cy)
+      self.y = cy - height / 2
+    end
   end
 end
