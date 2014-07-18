8d5648388b107389564cb4fe5531027055a6e078
IceDragon200 <mistdragon100@gmail.com>
2014-07-18 14:58:26 -0500

    The mouse can now move the camera by approaching the egde of the screen
    Still a bit clunky

--- scripts/states/map_editor.rb
+++ scripts/states/map_editor.rb
@@ -3,11 +3,43 @@ require "scripts/states/map_editor/model"
 require "scripts/states/map_editor/view"
 require "scripts/states/map_editor/mode_stack"
 
+class RectEdgePressureDetector
+  attr_accessor :rect
+  attr_accessor :range
+  attr_accessor :easer
+
+  def initialize(rect, range=96, easer=Easer::Linear)
+    @range = range
+    @easer = easer
+    @rect = rect
+  end
+
+  def calc(position)
+    result = Vector2.new(0, 0)
+    return result unless @rect.inside?(position)
+    rel = position - @rect.xy
+    if rel.x < range
+      result.x = 1 - (rel.x / range.to_f)
+    elsif rel.x < @rect.width && rel.x >= (@rect.width-@range)
+      result.x = -(1 + (rel.x-@rect.width) / range)
+    end
+    if rel.y < range
+      result.y = 1 - (rel.y / range.to_f)
+    elsif rel.y < @rect.height && rel.y >= (@rect.height-@range)
+      result.y = -(1 + (rel.y-@rect.height) / range)
+    end
+    result.x = @easer.ease(0, 1, result.x)
+    result.y = @easer.ease(0, 1, result.y)
+    -result
+  end
+end
+
 module ES
   module States
     class MapEditor < Base
       def init
         super
+        @edge_pressure = RectEdgePressureDetector.new(Screen.rect, 24)
         @control_map = ES.cache.controlmap("map_editor.yml")
 
         @model = MapEditorModel.new
@@ -384,6 +416,8 @@ module ES
       end
 
       def update(delta)
+        pressure = @edge_pressure.calc(Input::Mouse.position)
+        @model.cam_cursor.position += pressure.xyz * delta * 8
         @view.update(delta)
         @controller.update(delta)
         if @mode.is?(:edit)
