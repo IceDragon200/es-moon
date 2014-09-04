284740d928601541d029d687a5589c0feb692b77
Corey Powell <mistdragon100@gmail.com>
2014-09-03 20:06:09 -0500

    Removed duck typing from data

--- build/data.rb
+++ build/data.rb
@@ -2,111 +2,14 @@
 
 # EDOS data building
 $: << File.expand_path("../", File.dirname(__FILE__))
+$: << File.expand_path("../modules", File.dirname(__FILE__))
 
 require "yaml"
 
 require 'core/core_ext'
 require 'core/data_model'
 
-# Duck typing
-module Moon
-  class Vector2 < Moon::DataModel::Metal
-
-    field :x,      type: Numeric, default: 0.0
-    field :y,      type: Numeric, default: 0.0
-
-    def initialize(*args, &block)
-      case args.size
-      when 1
-        super args.first, &block
-      when 2
-        x, y = *args
-        super x: x, y: y, &block
-      else
-        super x: 0.0, y: 0.0, &block
-      end
-    end
-
-    def to_a
-      return x, y
-    end
-  end
-  class Vector3 < Moon::DataModel::Metal
-
-    field :x,      type: Numeric, default: 0.0
-    field :y,      type: Numeric, default: 0.0
-    field :z,      type: Numeric, default: 0.0
-
-    alias :r :x
-    alias :r= :x=
-    alias :g :y
-    alias :g= :y=
-    alias :b :z
-    alias :b= :z=
-
-    def initialize(*args, &block)
-      case args.size
-      when 1
-        super args.first, &block
-      when 3
-        x, y, z = *args
-        super x: x, y: y, z: z, &block
-      else
-        super x: 0.0, y: 0.0, z: 0.0, &block
-      end
-    end
-
-    def to_a
-      return x, y, z
-    end
-
-  end
-  class Vector4 < Moon::DataModel::Metal
-
-    field :x,      type: Numeric, default: 0.0
-    field :y,      type: Numeric, default: 0.0
-    field :z,      type: Numeric, default: 0.0
-    field :w,      type: Numeric, default: 0.0
-
-    alias :r :x
-    alias :r= :x=
-    alias :g :y
-    alias :g= :y=
-    alias :b :z
-    alias :b= :z=
-    alias :a :w
-    alias :a= :w=
-
-    def initialize(*args, &block)
-      case args.size
-      when 1
-        super args.first, &block
-      when 4
-        x, y, z, w = *args
-        super x: x, y: y, z: z, w: w, &block
-      else
-        super x: 0.0, y: 0.0, z: 0.0, w: 0.0, &block
-      end
-    end
-
-    def to_a
-      return x, y, z, w
-    end
-
-  end
-  class Rect < Moon::DataModel::Metal
-
-    field :x,      type: Integer, default: 0
-    field :y,      type: Integer, default: 0
-    field :width,  type: Integer, default: 0
-    field :height, type: Integer, default: 0
-
-    def initialize(x, y, width, height, &block)
-      super x: x, y: y, width: width, height: height, &block
-    end
-
-  end
-end
+require 'duck/load'
 
 require 'core/vector2'
 require 'core/vector3'
