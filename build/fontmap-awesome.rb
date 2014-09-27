3207f9cd42b484652c6c2336f55e77c064f66ef2
Corey Powell <mistdragon100@gmail.com>
2014-09-26 19:43:48 -0500

    Updated font-awesome

--- build/fontmap-awesome.rb
+++ build/fontmap-awesome.rb
@@ -2,11 +2,13 @@
 require "yaml"
 require "fileutils"
 
+font_awesome_ver = ARGV[0]
+
 result = {
   "" => ""
 }
 
-vars = File.read File.expand_path("font-awesome-4.1.0/less/variables.less", File.dirname(__FILE__))
+vars = File.read File.expand_path("font-awesome-#{font_awesome_ver}/less/variables.less", File.dirname(__FILE__))
 vars.scan(/fa-var-(\S+):\s+\"\\f(.+)\";/) do |name, value|
   result[name] = ["F#{value}".to_i(16)].pack "U*"
 end
@@ -14,6 +16,6 @@ end
 filename = File.expand_path "../data/charmap/awesome.yml", File.dirname(__FILE__)
 File.write filename, YAML.dump(result)
 
-source = File.expand_path "font-awesome-4.1.0/fonts/fontawesome-webfont.ttf", File.dirname(__FILE__)
+source = File.expand_path "font-awesome-#{font_awesome_ver}/fonts/fontawesome-webfont.ttf", File.dirname(__FILE__)
 target = File.expand_path "../resources/fonts/fontawesome-webfont.ttf", File.dirname(__FILE__)
 FileUtils::Verbose.cp source, target
