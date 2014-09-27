f98cc9ccabd8e301745f99f870e1b80cac10ffc9
Corey Powell <mistdragon100@gmail.com>
2014-09-27 13:46:15 -0500

    data_model > models

---
+++ scripts/models.rb
@@ -0,0 +1,13 @@
+require 'scripts/models/metal-ext'
+require 'scripts/models/base-ext'
+##
+require 'scripts/models/chunk_head'
+require 'scripts/models/tileset_head'
+##
+require 'scripts/models/chunk'
+require 'scripts/models/entity'
+require 'scripts/models/map'
+require 'scripts/models/tile_data'
+##
+require 'scripts/models/character'
+require 'scripts/models/popup'
