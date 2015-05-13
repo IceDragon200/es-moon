b5f6ad96d6414444b5fbbeb1b8267ba2236706db
Corey Powell <mistdragon100@gmail.com>
2015-05-13 10:47:54 -0500

    Split scripts/load.rb into load.rb and main.rb

--- scripts/load.rb
+++ scripts/load.rb
@@ -3,35 +3,20 @@
 require 'scripts/mixin'
 require 'scripts/core_ext'
 require 'scripts/version'
+require 'scripts/core'
 require 'scripts/caches'
+require 'scripts/const'
+require 'scripts/database'
+require 'scripts/models'
+require 'scripts/helpers'
 require 'scripts/entity_system'
 require 'scripts/renderers'
 require 'scripts/ui'
-require 'scripts/core'
-require 'scripts/const'
-require 'scripts/models'
-require 'scripts/database'
 require 'scripts/states'
-require 'scripts/helpers'
 require 'scripts/adapters'
+require 'scripts/aux'
 ##
 require 'scripts/test'
 require 'scripts/data'
 
-TextureCache = ES::TextureCacheClass.new
-FontCache = ES::FontCacheClass.new
-DataCache = ES::DataCacheClass.new
-
-State.push States::Shutdown
-State.push States::Title
-#State.push Roadmap::StateGridBasedCharacterMovement
-#State.push Roadmap::StateCharacterMovement
-#State.push Roadmap::StateDisplaySpriteOnTilemap
-#State.push Roadmap::StateDisplayTilemapWithChunks
-#State.push Roadmap::StateDisplayChunk
-#State.push Roadmap::StateDisplaySpriteOnScreen
-#State.push StateMusicLayering
-#State.push StateCharacterWalkTest
-#State.push StateTypingTest
-#State.push StateUITest01
-#State.push ES::States::Splash
+require 'scripts/main'
---
+++ scripts/main.rb
@@ -0,0 +1,57 @@
+TextureCache = ES::TextureCacheClass.new
+FontCache = ES::FontCacheClass.new
+DataCache = ES::DataCacheClass.new
+
+class StateBootstrap
+  class StateManager < Moon::StateManager
+
+  end
+
+  attr_reader :engine
+
+  def initialize(engine)
+    @engine = engine
+    @state_manager = StateManager.new @engine
+    load_config
+    setup_states
+  end
+
+  def load_config
+    @config = YAML.load_file('config.yml').symbolize_keys
+  end
+
+  def setup_states
+    @state_manager.push States::Shutdown
+    @state_manager.push States::Title
+    # Test Map
+    #@state_manager.push States::FpMap
+    ## Roadmap tests
+    #@state_manager.push Roadmap::StateGridBasedCharacterMovement
+    #@state_manager.push Roadmap::StateCharacterMovement
+    #@state_manager.push Roadmap::StateDisplaySpriteOnTilemap
+    #@state_manager.push Roadmap::StateDisplayTilemapWithChunks
+    #@state_manager.push Roadmap::StateDisplayChunk
+    #@state_manager.push Roadmap::StateDisplaySpriteOnScreen
+
+    #@state_manager.push StateMusicLayering
+    #@state_manager.push StateCharacterWalkTest
+    #@state_manager.push StateTypingTest
+    #@state_manager.push StateUITest01
+
+    # Splash screens
+    if @config[:splash] == 'true'
+      @state_manager.push States::EsSplash
+      @state_manager.push States::MoonSplash
+      @state_manager.push States::MrubySplash
+    end
+  end
+
+  def step(delta)
+    @state_manager.step delta
+  end
+end
+
+def step(e, delta)
+  @state_main ||= StateBootstrap.new(e)
+  @state_main.step delta
+end
