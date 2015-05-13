8e91831a35a478df9e57c5b163bbbb968fef9b48
Corey Powell <mistdragon100@gmail.com>
2015-05-13 10:58:46 -0500

    Updated TilePaletteEditor

--- scripts/states/tile_palette_editor.rb
+++ scripts/states/tile_palette_editor.rb
@@ -4,9 +4,9 @@ class TilePalettePanel < Moon::RenderContainer
 
   def initialize
     super
-    @background = Moon::Sprite.new("resources/ui/grid_32x32_ff777777.png")
-    @cursor = Moon::Sprite.new("resources/ui/map_editor_cursor.png")
-    @text = Moon::Text.new("", FontCache.font("uni0553", 8))
+    @background = Moon::Sprite.new('resources/ui/grid_32x32_ff777777.png')
+    @cursor = Moon::Sprite.new('resources/ui/map_editor_cursor.png')
+    @text = Moon::Text.new('', FontCache.font('uni0553', 8))
     @tile_palette = nil
 
     @text.align = :right
@@ -20,7 +20,7 @@ class TilePalettePanel < Moon::RenderContainer
   def refresh_tile_palette
     tileset = @tile_palette.tileset
     texture = TextureCache.tileset(tileset.filename)
-    @spritesheet = Moon::Spritesheet.new(texture, tileset.cell_width, tileset.cell_height)
+    @spritesheet = Moon::Spritesheet.new(texture, tileset.cell_w, tileset.cell_h)
   end
 
   def update(delta)
@@ -30,7 +30,7 @@ class TilePalettePanel < Moon::RenderContainer
   def render(x=0, y=0, z=0, options={})
     if @spritesheet
       px, py, pz = *(@position + [x, y, z])
-      cw, ch = @spritesheet.cell_width, @spritesheet.cell_height
+      cw, ch = @spritesheet.cell_w, @spritesheet.cell_h
       cols = @tile_palette.columns
       rows = (@tile_palette.tiles.size / cols).to_i + [1, @tile_palette.tiles.size % cols].min
       @background.clip_rect = Rect.new(0, 0, cols * cw, rows * ch)
@@ -42,8 +42,8 @@ class TilePalettePanel < Moon::RenderContainer
       end
 
       curpos = @cursor_position * [cw, ch, 1] + [px, py, pz]
-      txtpos = curpos + [@cursor.width-8, 0, 0]
-      txtpos2 = txtpos + [0, @cursor.height-12, 0]
+      txtpos = curpos + [@cursor.w - 8, 0, 0]
+      txtpos2 = txtpos + [0, @cursor.h - 12, 0]
 
       tile_index = (@cursor_position.x + @cursor_position.y * cols).to_i
 
@@ -63,8 +63,8 @@ class TilesetPanel < Moon::RenderContainer
 
   def initialize
     super
-    @background = Sprite.new("media/ui/grid_32x32_ff777777.png")
-    @cursor = Sprite.new("media/ui/map_editor_cursor.png")
+    @background = Sprite.new('media/ui/grid_32x32_ff777777.png')
+    @cursor = Sprite.new('media/ui/map_editor_cursor.png')
     @tileset = nil
   end
 
@@ -88,11 +88,11 @@ class TilesetPanel < Moon::RenderContainer
   def render(x=0, y=0, z=0, options={})
     if @tileset_sprite
       px, py, pz = *(@position + [x, y, z])
-      cw, ch = @tileset.cell_width, @tileset.cell_height
+      cw, ch = @tileset.cell_w, @tileset.cell_h
       page_y = @cursor_position.y * ch
 
-      @tileset_sprite.clip_rect = Rect.new(0, page_y, @tileset_sprite.texture.width, Screen.height - @position.y)
-      @background.clip_rect = Rect.new(0, @tileset_sprite.clip_rect.y%ch, @tileset_sprite.width, @tileset_sprite.height)
+      @tileset_sprite.clip_rect = Rect.new(0, page_y, @tileset_sprite.texture.w, screen.h - @position.y)
+      @background.clip_rect = Rect.new(0, @tileset_sprite.clip_rect.y%ch, @tileset_sprite.w, @tileset_sprite.h)
       @background.render(px, py, pz)
       @tileset_sprite.render(px, py, pz)
       @cursor.render(*(@cursor_position * [cw, ch, 1] + [px, py-page_y, pz]))
@@ -106,24 +106,20 @@ class TilePaletteInfoPanel < Moon::RenderContainer
 
   def initialize
     super
-    @text = Moon::Text.new("", FontCache.font("uni0553", 16))
+    @text = Moon::Text.new('', FontCache.font('uni0553', 16))
     @tile_palette = nil
     add(@text)
   end
 
-  def update(delta)
+  def update_content(delta)
     if @tile_palette
       @text.string = "columns: #{@tile_palette.columns}\n" +
                      "rows: #{@tile_palette.rows}\n" +
                      "\n" +
                      "\n"
     else
-      @text.string = ""
+      @text.string = ''
     end
-    super
-  end
-
-  def render(x=0, y=0, z=0, options={})
     super
   end
 end
@@ -137,10 +133,6 @@ class TilePaletteEditorModel < State::ModelBase
   field :tileset_cursor, type: TileCursor, default: proc{|t|t.new}
   field :tile_palette, type: ES::EditorTilePalette, allow_nil: true, default: nil
   field :tileset,      type: ES::Tileset, allow_nil: true, default: nil
-
-  def update_model(delta)
-    super
-  end
 end
 
 class TilePaletteEditorView < State::ViewBase
@@ -151,7 +143,7 @@ class TilePaletteEditorView < State::ViewBase
     @info_panel = TilePaletteInfoPanel.new
 
     @tileset_panel.position.set(0, 128, 0)
-    @info_panel.position.set(Screen.width - 128, 0, 0)
+    @info_panel.position.set(engine.screen.w - 128, 0, 0)
 
     add(@tile_palette_panel)
     add(@tileset_panel)
@@ -217,7 +209,7 @@ module States
       @view = TilePaletteEditorView.new(@model)
       @controller = TilePaletteEditorController.new(@model, @view)
 
-      @model.tile_palette = cvar["tile_palette"]
+      @model.tile_palette = cvar['tile_palette']
       @model.tileset = @model.tile_palette.tileset
       @view.refresh
 
@@ -226,48 +218,48 @@ module States
 
     def register_input
       # move tileset cursor
-      @input.on :press, :h do
+      input.on :press, :h do
         @controller.move_tileset_cursor(-1, 0)
       end
 
-      @input.on :press, :j do
+      input.on :press, :j do
         @controller.move_tileset_cursor(0, 1)
       end
 
-      @input.on :press, :k do
+      input.on :press, :k do
         @controller.move_tileset_cursor(0, -1)
       end
 
-      @input.on :press, :l do
+      input.on :press, :l do
         @controller.move_tileset_cursor(1, 0)
       end
 
-      @input.on :press, :z do
+      input.on :press, :z do
         @controller.add_to_palette
       end
 
-      @input.on :press, :x do
+      input.on :press, :x do
         @controller.remove_from_palette
       end
 
-      @input.on :press, :c do
+      input.on :press, :c do
         @controller.jump_to_tile
       end
 
       # move palette cursor
-      @input.on :press, :left do
+      input.on :press, :left do
         @controller.move_palette_cursor(-1, 0)
       end
 
-      @input.on :press, :down do
+      input.on :press, :down do
         @controller.move_palette_cursor(0, 1)
       end
 
-      @input.on :press, :up do
+      input.on :press, :up do
         @controller.move_palette_cursor(0, -1)
       end
 
-      @input.on :press, :right do
+      input.on :press, :right do
         @controller.move_palette_cursor(1, 0)
       end
     end
