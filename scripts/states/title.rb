127bf7e4353b9ab3569324c438edd14648197fb2
Corey Powell <mistdragon100@gmail.com>
2015-05-13 11:10:07 -0500

    Updated scripts/states/title

--- scripts/states/title.rb
+++ scripts/states/title.rb
@@ -2,55 +2,69 @@ module States
   class Title < Base
     def init
       super
+      create_background
       create_title_menu
 
-      #@title_texture = TextureCache.system "title_text.png"
+      #@title_texture = TextureCache.system 'title_text.png'
       #@title_sprite = Moon::Sprite.new(@title_texture)
+    end
 
-      register_events
+    def start
+      super
+      t = TweenScheduler.new(scheduler)
+      t.tween_obj @background.sprite, :opacity, to: 1.0, duration: '2s', easer: Moon::Easing::Linear
+    end
+
+    def create_background
+      tex = TextureCache.background 'title.png'
+      @background = Moon::Sprite.new(tex).to_proxy_sprite
+      @background.sprite.opacity = 0.0
+
+      @gui.add @background
     end
 
     def create_title_menu
       @title_menu = UI::TitleMenu.new
-      @title_menu.add_entry(:newgame, "New Game")
-      @title_menu.add_entry(:continue, "Continue")
-      @title_menu.add_entry(:map_viewer, "Map Viewer")
-      @title_menu.add_entry(:map_editor, "Map Editor")
-      @title_menu.add_entry(:quit, "Quit")
+      @title_menu.tag('#menu')
+      @title_menu.add_entry(:newgame, 'New Game')
+      @title_menu.add_entry(:continue, 'Continue')
+      @title_menu.add_entry(:map_viewer, 'Map Viewer')
+      @title_menu.add_entry(:map_editor, 'Map Editor')
+      @title_menu.add_entry(:quit, 'Quit')
       #
-      @title_menu.align!("center", Moon::Screen.rect)
+      @title_menu.align!('center', screen.rect)
       @title_menu.position = Moon::Vector3.new(8, 4, 0)
 
       @gui.add @title_menu
     end
 
-    def register_events
-      @input.on :press, :up do
+    def register_input
+      input.on :press, :up do
         @title_menu.index -= 1
       end
 
-      @input.on :press, :down do
+      input.on :press, :down do
         @title_menu.index += 1
       end
 
-      @input.on :press, :enter, :z do
+      input.on :press, :enter, :z do
         on_title_menu_accept
       end
 
-      #@input.on :press, :x do
+      #input.on :press, :x do
       #end
     end
 
     def on_title_menu_accept
       case @title_menu.current_item[:id]
       when :newgame
-        State.change States::Map
+        state_manager.change States::NewGame
       when :map_viewer
-        State.change States::MapViewer
+        state_manager.push States::MapViewer
       when :map_editor
-        State.change EsMapEditor
+        state_manager.push EsMapEditor
       when :quit
-        State.pop
+        state_manager.pop
       end
     end
   end
