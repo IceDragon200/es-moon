830ff9c583f74045b1cd6789839e596f18ea0a5d
Corey Powell <mistdragon100@gmail.com>
2015-05-13 10:41:18 -0500

    Updated caches

--- scripts/caches.rb
+++ scripts/caches.rb
@@ -9,61 +9,79 @@ module ES
     end
   end
 
-  class EsCacheClass < Moon::CacheBase
+  class CacheClass < Moon::CacheBase
   end
 
-  class TextureCacheClass < EsCacheClass
+  class TextureCacheClass < CacheClass
     private def load_texture(filename)
       Moon::Texture.new(filename)
     end
 
+    cache def resource(filename)
+      load_texture('resources/' + filename)
+    end
+
     cache def block(filename)
-      load_texture("resources/blocks/" + filename)
+      load_resource('blocks/' + filename)
     end
 
     cache def icon(filename)
-      load_texture("resources/icons_64x64/" + filename)
+      load_resource('icons_64x64/' + filename)
     end
 
     cache def bmpfont(filename)
-      load_texture("resources/bmpfont/" + filename)
+      load_resource('bmpfont/' + filename)
     end
 
     cache def tileset(filename)
-      load_texture("resources/tilesets/" + filename)
+      load_resource('tilesets/' + filename)
     end
 
     cache def system(filename)
-      load_texture("resources/system/" + filename)
+      load_resource('system/' + filename)
+    end
+
+    cache def gauge(filename)
+      load_resource('gauges/' + filename)
+    end
+
+    cache def ui(filename)
+      load_resource('ui/' + filename)
+    end
+
+    cache def background(filename)
+      load_resource('backgrounds/' + filename)
     end
   end
 
-  class FontCacheClass < EsCacheClass
-    def post_init
+  class FontCacheClass < CacheClass
+    def post_initialize
       super
       @aliases = {}
 
-      @aliases["awesome"] = "fontawesome-webfont.ttf"
+      @aliases['awesome'] = 'fontawesome-webfont.ttf'
 
-      @aliases["foundation"] = "general_foundicons.ttf"
-      @aliases["foundation_enclosed"] = "general_enclosed_foundicons.ttf"
+      @aliases['foundation'] = 'general_foundicons.ttf'
+      @aliases['foundation_enclosed'] = 'general_enclosed_foundicons.ttf'
 
-      @aliases["uni0553"] = "uni0553/uni0553-webfont.ttf"
-      @aliases["uni0554"] = "uni0554/uni0554-webfont.ttf"
-      @aliases["uni0563"] = "uni0563/uni0563-webfont.ttf"
-      @aliases["uni0564"] = "uni0564/uni0564-webfont.ttf"
+      @aliases['uni0553'] = 'uni0553/uni0553-webfont.ttf'
+      @aliases['uni0554'] = 'uni0554/uni0554-webfont.ttf'
+      @aliases['uni0563'] = 'uni0563/uni0563-webfont.ttf'
+      @aliases['uni0564'] = 'uni0564/uni0564-webfont.ttf'
 
-      @aliases["ipaexg"] = "ipaexg00201/ipaexg.ttf"
+      @aliases['ipaexg'] = 'ipaexg00201/ipaexg.ttf'
 
-      @aliases["vera"] = "vera/Vera.ttf"
-      @aliases["vera_mono"] = "vera/VeraMono.ttf"
-      @aliases["vera_mono_bold_italic"] = "vera/VeraMoBI.ttf"
-      @aliases["vera_mono_bold"] = "vera/VeraMoBd.ttf"
-      @aliases["vera_mono_italic"] = "vera/VeraMoIt.ttf"
+      @aliases['vera'] = 'vera/Vera.ttf'
+      @aliases['vera_mono'] = 'vera/VeraMono.ttf'
+      @aliases['vera_mono_bold_italic'] = 'vera/VeraMoBI.ttf'
+      @aliases['vera_mono_bold'] = 'vera/VeraMoBd.ttf'
+      @aliases['vera_mono_italic'] = 'vera/VeraMoIt.ttf'
+
+      @aliases['system'] = @aliases['uni0553']
     end
 
     cache def f(filename, size)
-      Moon::Font.new("resources/fonts/" + filename, size)
+      Moon::Font.new('resources/fonts/' + filename, size)
     end
 
     def font(subname, size)
@@ -71,17 +89,19 @@ module ES
     end
   end
 
-  class DataCacheClass < EsCacheClass
+  class DataCacheClass < CacheClass
     cache def palette
-      FetchOnlyHash.new(PaletteParser.load_palette(DataSerializer.load_file("palette")))
+      data = Moon::DataSerializer.load_file('palette')
+      palette = Moon::PaletteParser.load_palette(data)
+      FetchOnlyHash.new(palette)
     end
 
     cache def controlmap(filename)
-      FetchOnlyHash.new(DataSerializer.load_file("controlmaps/#{filename}"))
+      FetchOnlyHash.new(Moon::DataSerializer.load_file("controlmaps/#{filename}"))
     end
 
     cache def charmap(filename)
-      DataSerializer.load_file("charmap/#{filename}")
+      Moon::DataSerializer.load_file("charmap/#{filename}")
     end
   end
 end
