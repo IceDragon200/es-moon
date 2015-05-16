8626801596393c0c0e4aef5fd6f10d64f09029d7
Corey Powell <mistdragon100@gmail.com>
2015-05-16 13:04:26 -0500

    Trimmed up comments

--- scripts/models/metal-ext.rb
+++ scripts/models/metal-ext.rb
@@ -3,28 +3,23 @@ module Moon
     class Metal
       include Database::Record
 
-      ###
-      # When questionin the model about its properties this is used.
+      # When querying the model about its properties this is used.
+      #
       # @param [String|Symbol] key
       # @param [Object] value
       # @return [Boolean]
-      ###
       def query(key, value)
         # tags and meta are special cases
         case key.to_s
-        ###
         # EG:
         # tags: ["red", "herb", "thing"]
         # tag: "herb"
-        ###
         when 'tags', 'tag'
           value = [value] unless value.is_a?(Array)
           (meta & value) == value
-        ###
         # NOTE* Key Symbols will be converted to Strings
         # EG:
         # meta: { x: 'hi' }
-        ###
         when 'meta'
           value.all? { |k, v| @meta[k.to_s] == v }
         else
@@ -32,17 +27,12 @@ module Moon
         end
       end
 
-      ###
       # @return [String]
-      ###
       def self.basepath
         'data/'
       end
 
-      ###
       # @param [String] rootname
-      #   @optional
-      ###
       def save_file(rootname = nil)
         warn "no uri set, skipping saving of #{self["name"]}" unless self['uri']
 
