5cca4fb24bd60f94fc42f055bf0bbc304addc2b4
Corey Powell <mistdragon100@gmail.com>
2015-05-17 20:07:41 -0500

    Configured window width and height

--- application.rb
+++ application.rb
@@ -1,9 +1,16 @@
 class Application
+  attr_accessor :state_main
+
   def start(engine)
     @state_main = StateBootstrap.new engine
   end
 
   def step(_, delta)
     @state_main.step delta
+  end
+
+  def configure(engine)
+    engine.config[:width] = 1024
+    engine.config[:height] = 640
   end
 end
