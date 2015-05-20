8d22c4d53b110a0e67648422d53b569032a97f89
Corey Powell <mistdragon100@gmail.com>
2015-05-20 09:56:53 -0500

    TileData model, default data_position and chunk_data_position, addded a valid field

--- scripts/models/tile_data.rb
+++ scripts/models/tile_data.rb
@@ -1,9 +1,10 @@
 module ES
   class TileData < Moon::DataModel::Base
+    field :valid,               type: Boolean,       default: false
     field :passage,             type: Integer,       default: 0
     array :tile_ids,            type: Integer
     field :chunk,               type: EditorChunk,   allow_nil: true, default: nil
-    field :data_position,       type: Moon::Vector3, allow_nil: true
-    field :chunk_data_position, type: Moon::Vector3, allow_nil: true
+    field :data_position,       type: Moon::Vector3, default: Moon::Vector3.zero
+    field :chunk_data_position, type: Moon::Vector3, default: Moon::Vector3.zero
   end
 end
