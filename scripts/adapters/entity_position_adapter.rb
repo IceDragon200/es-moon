eba8edc272555886ea985ace1b35c0f51b525b6c
Corey Powell <mistdragon100@gmail.com>
2015-05-23 23:48:35 -0500

    Updated EntityPositionAdapter
    The adapter should convert an Entities Vector3 position to a Vector2

--- scripts/adapters/entity_position_adapter.rb
+++ scripts/adapters/entity_position_adapter.rb
@@ -1,14 +1,12 @@
 class EntityPositionAdapter
   def initialize(obj)
     @obj = obj
-    @position = Moon::Vector3.new(0, 0, 0)
+    @position = Moon::Vector2.new(0, 0)
   end
 
   def position
-    pos = @obj[:transform].position
-    @position.x = pos.x
-    @position.y = pos.y
-    @position.z = pos.z
+    p = @obj[:transform].position
+    @position.set p.x, p.y
     @position
   end
 end
