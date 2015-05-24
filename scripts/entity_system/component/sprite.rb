f85d144c63d8f90bd31485e6235c0e5fd6c6ea27
Corey Powell <mistdragon100@gmail.com>
2015-05-23 23:49:10 -0500

    Updated entity system SpriteComponent

--- scripts/entity_system/component/sprite.rb
+++ scripts/entity_system/component/sprite.rb
@@ -3,6 +3,8 @@ class SpriteComponent
   include Moon::EntitySystem::Component
   register :sprite
 
+  field :filename,  type: String
+  field :clip_rect, type: Moon::Rect
   # last known rendered bounds
-  field :bounds, type: Moon::Cuboid, default: proc { |t| t.new }
+  field :bounds, type: Moon::Cuboid, default: proc { |t| t.model.new }
 end
