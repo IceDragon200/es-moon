c6613bd779001efb0819a96568862aea1e9a9bf8
Corey Powell <mistdragon100@gmail.com>
2015-06-03 21:35:42 -0500

    Use Enum::Team::Neutral instead of magic number

--- scripts/entity_system/component/team.rb
+++ scripts/entity_system/component/team.rb
@@ -2,5 +2,5 @@ class TeamComponent
   include Moon::EntitySystem::Component
   register :team
 
-  field :number, type: Integer, default: 0
+  field :number, type: Integer, default: Enum::Team::NEUTRAL
 end
