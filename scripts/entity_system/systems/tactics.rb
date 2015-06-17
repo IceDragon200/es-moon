7dd21b6bc63feebac906921d9e8d724ad3d33000
Corey Powell <mistdragon100@gmail.com>
2015-06-17 16:55:25 -0500

    Killed the case statement, created classes to replace it
    I don't think I've made much of an improvement...

--- scripts/entity_system/systems/tactics.rb
+++ scripts/entity_system/systems/tactics.rb
@@ -1,153 +1,277 @@
+module Phases
+  module Base
+    include Enum::TacticsPhase
+
+    def goto_and_idle(tactics, new_phase)
+      tactics.next_phase = new_phase
+      tactics.idle = true
+    end
+
+    # @param [TacticsComponent] tactics
+    def process(tactics, world, delta)
+    end
+  end
+
+  module Judge
+  end
+
+  class Idle
+    include Base
+  end
+
+  class BattleStart
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      tactics.battle_wt = 0
+      tactics.rounds = 0
+      tactics.subject_id = nil
+      # reset all entities wait_time to its maximum
+      world.filter(:wait_time) do |entity|
+        wt = entity[:wait_time]
+        wt.value = wt.max
+      end
+      goto_and_idle tactics, ROUND_NEXT
+    end
+  end
+
+  class RoundNext
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      tactics.rounds += 1
+      tactics.round_wt = tactics.round_wt_max
+      goto_and_idle tactics, ROUND_START
+    end
+  end
+
+  class RoundStart
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, NEXT_TICK
+    end
+  end
+
+  class NextTick
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      entities = world[:wait_time]
+      if entities.empty?
+        # there are no valid entities to conduct battle
+        goto_and_idle tactics, BATTLE_END
+      else
+        # grab the entity with the shortest wait_time
+        entity = entities.min_by { |e| e[:wait_time].value }
+        wt = entity[:wait_time]
+        diff = wt.value
+        r = tactics.round_wt - diff
+        # determine if the entity can have its turn within the current round
+        if r < 0
+          # the entity would overstep the current round, set the diff as
+          # the remaining ticks in the round
+          diff = tactics.round_wt
+          tactics.subject_id = nil
+          goto_and_idle tactics, ROUND_END
+        else
+          # the entity can act in the current round
+          tactics.subject_id = entity.id
+          goto_and_idle tactics, TURN_START
+        end
+        # decrease all entities round_wt by the diff
+        entities.each do |entity|
+          entity[:wait_time].value -= diff
+        end
+        # decreate the round_wt by the diff
+        tactics.round_wt -= diff
+        # increase the battle_wt by the diff
+        tactics.battle_wt += diff
+      end
+    end
+  end
+
+  class TurnStart
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      if entity = world.get_entity_by_id(tactics.subject_id)
+        wt = entity[:wait_time]
+        #wt.reset = 0
+        wt.reset = wt.max
+      end
+      goto_and_idle tactics, ACTION_NEXT
+    end
+  end
+
+  class ActionNext
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      #goto_and_idle tactics, ACTION_MAKE
+      goto_and_idle tactics, TURN_END
+    end
+  end
+
+  class ActionMake
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, ACTION_PREPARE
+    end
+  end
+
+  class ActionPrepare
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, ACTION_EXECUTE
+    end
+  end
+
+  class ActionExecute
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, ACTION_JUDGE
+    end
+  end
+
+  class ActionJudge
+    include Base
+    include Judge
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, ACTION_NEXT
+      #goto_and_idle tactics, BATTLE_END
+    end
+  end
+
+  class TurnEnd
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      if entity = world.get_entity_by_id(tactics.subject_id)
+        wt = entity[:wait_time]
+        wt.value += wt.reset
+      end
+      goto_and_idle tactics, TURN_JUDGE
+    end
+  end
+
+  class TurnJudge
+    include Base
+    include Judge
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, NEXT_TICK
+      #goto_and_idle tactics, BATTLE_END
+    end
+  end
+
+  class RoundEnd
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      puts "Round End"
+      goto_and_idle tactics, ROUND_JUDGE
+    end
+  end
+
+  class RoundJudge
+    include Base
+    include Judge
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, ROUND_NEXT
+      #goto_and_idle tactics, BATTLE_END
+    end
+  end
+
+  class BattleEnd
+    include Base
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+      goto_and_idle tactics, BATTLE_JUDGE
+    end
+  end
+
+  class BattleJudge
+    include Base
+    include Judge
+
+    # (see Base#process)
+    def process(tactics, world, delta)
+    end
+  end
+
+  PHASE_TABLE = {
+    # oh great, glorified function mappings Yaaay.
+    Enum::TacticsPhase::IDLE           => Idle,
+    Enum::TacticsPhase::BATTLE_START   => BattleStart,
+    Enum::TacticsPhase::ROUND_NEXT     => RoundNext,
+    Enum::TacticsPhase::ROUND_START    => RoundStart,
+    Enum::TacticsPhase::NEXT_TICK      => NextTick,
+    Enum::TacticsPhase::TURN_START     => TurnStart,
+    Enum::TacticsPhase::ACTION_NEXT    => ActionNext,
+    Enum::TacticsPhase::ACTION_MAKE    => ActionMake,
+    Enum::TacticsPhase::ACTION_PREPARE => ActionPrepare,
+    Enum::TacticsPhase::ACTION_EXECUTE => ActionExecute,
+    Enum::TacticsPhase::ACTION_JUDGE   => ActionJudge,
+    Enum::TacticsPhase::TURN_END       => TurnEnd,
+    Enum::TacticsPhase::TURN_JUDGE     => TurnJudge,
+    Enum::TacticsPhase::ROUND_END      => RoundEnd,
+    Enum::TacticsPhase::ROUND_JUDGE    => RoundJudge,
+    Enum::TacticsPhase::BATTLE_END     => BattleEnd,
+    Enum::TacticsPhase::BATTLE_JUDGE   => BattleJudge,
+  }
+end
+
 class TacticsSystem
   include Moon::EntitySystem::System
   include Enum::TacticsPhase
-
   register :tactics
 
-  def battle_start(tactics)
-    tactics.battle_wt = 0
-    tactics.rounds = 0
-    tactics.subject = nil
-    tactics.phase = ROUND_NEXT
+  def post_initialize
+    super
+    initialize_phases
   end
 
-  def round_next(tactics)
-    tactics.rounds += 1
-    tactics.round_wt = tactics.round_wt_max
-    tactics.phase = NEXT_TICK
-  end
-
-  def next_tick(tactics)
-    entities = world[:wait_time]
-    if entities.empty?
-      # there are no valid entities to conduct battle
-      tactics.phase = BATTLE_END
-    else
-      # grab the entity with the shortest wait_time
-      entity = entities.min_by { |e| e[:wait_time].value }
-      wt = entity[:wait_time]
-      diff = wt.value
-      r = tactics.round_wt - diff
-      # determine if the entity can have its turn within the current round
-      if r < 0
-        # the entity would overstep the current round, set the diff as
-        # the remaining ticks in the round
-        diff = tactics.round_wt
-        tactics.subject = nil
-        tactics.phase = ROUND_END
-      else
-        # the entity can act in the current round
-        tactics.subject = entity.id
-        tactics.phase = TURN_START
-      end
-      # decrease all entities round_wt by the diff
-      entities.each do |entity|
-        entity[:wait_time].value -= diff
-      end
-      # decreate the round_wt by the diff
-      tactics.round_wt -= diff
-      # increase the battle_wt by the diff
-      tactics.battle_wt += diff
+  def initialize_phases
+    @phases = {}
+    Phases::PHASE_TABLE.each_pair do |key, klass|
+      @phases[key] = klass.new
     end
-  end
-
-  def turn_start(tactics)
-    tactics.phase = ACTION_NEXT
-  end
-
-  def action_next(tactics)
-    #tactics.phase = ACTION_MAKE
-    tactics.phase = TURN_END
-  end
-
-  def action_make(tactics)
-    tactics.phase = ACTION_PREPARE
-  end
-
-  def action_prepare(tactics)
-    tactics.phase = ACTION_EXECUTE
-  end
-
-  def action_execute(tactics)
-    tactics.phase = ACTION_JUDGE
-  end
-
-  def action_judge(tactics)
-    tactics.phase = ACTION_NEXT
-    #tactics.phase = BATTLE_END
-  end
-
-  def turn_end(tactics)
-    if entity = world.get_entity_by_id(tactics.subject)
-      wt = entity[:wait_time]
-      wt.value += wt.max
-    end
-    tactics.phase = TURN_JUDGE
-  end
-
-  def turn_judge(tactics)
-    tactics.phase = NEXT_TICK
-    #tactics.phase = BATTLE_END
-  end
-
-  def round_end(tactics)
-    puts "Round End"
-    tactics.phase = ROUND_JUDGE
-  end
-
-  def round_judge(tactics)
-    tactics.phase = ROUND_NEXT
-    #tactics.phase = BATTLE_END
-  end
-
-  def battle_end(tactics)
-    tactics.phase = BATTLE_JUDGE
-  end
-
-  def battle_judge(tactics)
   end
 
   def update(delta)
     # retrieve the tactics model
     world.filter(:tactics) do |entity|
       tactics = entity[:tactics]
-      case tactics.phase
-      when IDLE
-        # do nothing
-      when BATTLE_START
-        battle_start tactics
-      when ROUND_NEXT
-        round_next tactics
-      when ROUND_START
-        round_start tactics
-      when NEXT_TICK
-        next_tick tactics
-      when TURN_START
-        turn_start tactics
-      when ACTION_NEXT
-        action_next tactics
-      when ACTION_MAKE
-        action_make tactics
-      when ACTION_PREPARE
-        action_prepare tactics
-      when ACTION_EXECUTE
-        action_execute tactics
-      when ACTION_JUDGE
-        action_judge tactics
-      when TURN_END
-        turn_end tactics
-      when TURN_JUDGE
-        turn_judge tactics
-      when ROUND_END
-        round_end tactics
-      when ROUND_JUDGE
-        round_judge tactics
-      when BATTLE_END
-        battle_end tactics
-      when BATTLE_JUDGE
-        battle_judge tactics
-      else
-        puts "warning: Tactics is in an invalid phase!"
+      until tactics.idle
+        if tactics.next_phase != Enum::TacticsPhase::INVALID
+          tactics.phase = tactics.next_phase
+        end
+        if phase = @phases[tactics.phase]
+          phase.process(tactics, world, delta)
+        else
+          puts "warning: Tactics is in an invalid phase!"
+        end
       end
     end
   end
