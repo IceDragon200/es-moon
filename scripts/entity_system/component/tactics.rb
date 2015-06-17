2c83a49647613fac0774f884df418e85a379053d
Corey Powell <mistdragon100@gmail.com>
2015-06-17 17:08:54 -0500

    Updated TacticsComponent
    subject > subject_id

--- scripts/entity_system/component/tactics.rb
+++ scripts/entity_system/component/tactics.rb
@@ -2,10 +2,21 @@ class TacticalComponent
   include Moon::EntitySystem::Component
   register :tactics
 
-  field :subject,      field: String,  default: nil
+  # whether the tactics is currently idling, this causes the tactics system to
+  # skip the component for processing, ultimately halting the phase process.
+  field :idle,         field: Boolean, default: false
+  # current acting entity's ID
+  field :subject_id,   field: String,  default: nil
+  # current phase
   field :phase,        field: Integer, default: Enum::TacticsPhase::INVALID
+  # next phase to goto
+  field :next_phase,   field: Integer, default: Enum::TacticsPhase::INVALID
+  # rounds passed so far in the current battle
   field :rounds,       field: Integer, default: 0
+  # wait time left in the current round
   field :round_wt,     field: Integer, default: 0
-  field :round_wt_max, field: Integer, default: 16 * 500
+  # maximum wait time in a round
+  field :round_wt_max, field: Integer, default: 1000
+  # total wait time in the current battle
   field :battle_wt,    field: Integer, default: 0
 end
