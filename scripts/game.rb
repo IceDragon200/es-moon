02c1e86c8f71f161d2f64cae929b300ff735a303
Corey Powell <mistdragon100@gmail.com>
2015-08-25 11:25:51 -0500

    Updated Game model

--- scripts/game.rb
+++ scripts/game.rb
@@ -1,14 +1,15 @@
 require 'scripts/core/material'
 require 'scripts/caches'
-require 'scripts/models/editor_map'
+require 'scripts/models/map'
 require 'scripts/entity_system'
 
 class Game < Moon::DataModel::Metal
-  field :map,           type: ES::EditorMap,           default: nil
+  field :map,           type: ES::Map,                 default: nil
   field :world,         type: ES::EntitySystem::World, default: nil
   field :data_cache,    type: ES::DataCache,           default: ->(t, _) { t.model.new }
-  field :font_cache,    type: ES::FontCache,           default: ->(t, _) { t.model.new }
-  field :texture_cache, type: ES::TextureCache,        default: ->(t, _) { t.model.new }
+  field :font_cache,    type: ES::AssetCache,          default: ->(t, _) { t.model.new }
+  field :texture_cache, type: ES::AssetCache,          default: ->(t, _) { t.model.new }
   field :scheduler,     type: Moon::Scheduler,         default: ->(t, _) { t.model.new }
   dict  :materials,     key: String, value: Material
+  dict  :config, key: Symbol, value: Object
 end
