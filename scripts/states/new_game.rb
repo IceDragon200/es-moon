0d7684fb51107dcf2f17a2282dacbe26ad8f4405
Corey Powell <mistdragon100@gmail.com>
2015-08-25 11:28:37 -0500

    Dataman is no longer used for loading map data
    Grab a cahced on instead from the ES::Map record

--- scripts/states/new_game.rb
+++ scripts/states/new_game.rb
@@ -2,7 +2,7 @@ module States
   # Used for preparing a new game
   class NewGame < Base
     def invoke
-      game.map = Dataman.load_editor_map(uri: '/maps/school/f1')
+      game.map = ES::Map.find_by(uri: '/maps/school/f1')
       state_manager.change States::Map
     end
   end
