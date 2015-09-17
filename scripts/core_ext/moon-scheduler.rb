89219d59f16729626a362294af0c66e485c92d20
Corey Powell <mistdragon100@gmail.com>
2015-09-17 13:46:59 -0500

    Updated core_ext/moon-scheduler transition comment

--- scripts/core_ext/moon-scheduler.rb
+++ scripts/core_ext/moon-scheduler.rb
@@ -1,5 +1,9 @@
 module Moon
   class Scheduler
+    # Creates a new Transition job in the scheduler, see {Moon::Transition},
+    # and (Moon::Scheduler#add) for parameters and details.
+    #
+    # @return [Moon::Transition]
     def transition(*args, &block)
       add Moon::Transition.new(*args, &block)
     end
