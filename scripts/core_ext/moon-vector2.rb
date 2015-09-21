1a9e8fd91dd9181dcaf76fa9b3d569fa2728ece0
Corey Powell <mistdragon100@gmail.com>
2015-09-21 15:11:13 -0500

    Added Vector2, Vector3 #zero! and #to_int_ary

--- scripts/core_ext/moon-vector2.rb
+++ scripts/core_ext/moon-vector2.rb
@@ -1,5 +1,20 @@
 module Moon
   class Vector2
+    # Returns the properties of the vector as an Array of Integers
+    #
+    # @return [Array<Integer>]
+    def to_int_ary
+      [x.to_i, y.to_i]
+    end
+
+    # Sets the properties of the vector to 0
+    #
+    # @return [self]
+    def zero!
+      set(0, 0)
+      self
+    end
+
     def reduce(*args)
       rx, ry = *Vector2.extract(args.size > 1 ? args : args.first)
       Vector2.new (x / rx).floor * rx, (y / ry).floor * ry
--- scripts/core_ext/moon-vector3.rb
+++ scripts/core_ext/moon-vector3.rb
@@ -1,5 +1,20 @@
 module Moon
   class Vector3
+    # Returns the properties of the vector as an Array of Integers
+    #
+    # @return [Array<Integer>]
+    def to_int_ary
+      [x.to_i, y.to_i, z.to_i]
+    end
+
+    # Sets the properties of the vector to 0
+    #
+    # @return [self]
+    def zero!
+      set(0, 0, 0)
+      self
+    end
+
     def reduce(*args)
       rx, ry, rz = *Vector3.extract(args.size > 1 ? args : args.first)
       Vector3.new (x / rx).floor * rx, (y / ry).floor * ry, (z / rz).floor * rz
