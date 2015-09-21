d0852c455eb1f4a867974ce23382768a97291b8f
Corey Powell <mistdragon100@gmail.com>
2015-09-21 12:07:58 -0500

    Moved Moon::DataModel::Metal#query to Moon::DataModel::Base

--- scripts/models/metal-ext.rb
+++ scripts/models/metal-ext.rb
@@ -5,30 +5,6 @@ module Moon
     class Metal
       include Database::Queryable
 
-      # When querying the model about its properties this is used.
-      #
-      # @param [String|Symbol] key
-      # @param [Object] value
-      # @return [Boolean]
-      def query(key, value)
-        # tags and meta are special cases
-        case key.to_s
-        # EG:
-        # tags: ["red", "herb", "thing"]
-        # tag: "herb"
-        when 'tags', 'tag'
-          value = [value] unless value.is_a?(Array)
-          (meta & value) == value
-        # NOTE* Key Symbols will be converted to Strings
-        # EG:
-        # meta: { x: 'hi' }
-        when 'meta'
-          value.all? { |k, v| @meta[k.to_s] == v }
-        else
-          super key, value
-        end
-      end
-
       # @return [String]
       def self.basepath
         'data/'
