ceb12ba486b860f4ca36b2556356c58f1b560f20
Corey Powell <mistdragon100@gmail.com>
2015-09-21 22:01:32 -0500

    Added aliases for some fields in the Game object
    The old fields will be renamed later

--- scripts/game.rb
+++ scripts/game.rb
@@ -6,13 +6,16 @@ require 'scripts/entity_system'
 class Game < Moon::DataModel::Metal
   field :map,           type: Models::Map,             default: nil
   field :world,         type: ES::EntitySystem::World, default: nil
-  field :data_cache,    type: ES::DataCache,           default: ->(t, _) { t.model.new('Data') }
+  field :data_cache,    type: ES::DataCache,           default: ->(t, _) { t.model.new('Data') }     # deprecated, use database instead
   field :database,      type: ES::AssetCache,          default: ->(t, _) { t.model.new('Database') }
-  field :font_cache,    type: ES::AssetCache,          default: ->(t, _) { t.model.new('Fonts') }
-  field :texture_cache, type: ES::AssetCache,          default: ->(t, _) { t.model.new('Textures') }
+  field :font_cache,    type: ES::AssetCache,          default: ->(t, _) { t.model.new('Fonts') } # deprecated accessor, use fonts instead
+  field :texture_cache, type: ES::AssetCache,          default: ->(t, _) { t.model.new('Textures') } # deprecated accessor, use textures instead
   field :scheduler,     type: Moon::Scheduler,         default: ->(t, _) { t.model.new }
   dict  :materials,     key: String, value: Material
   dict  :config, key: Symbol, value: Object
+
+  alias :fonts :font_cache
+  alias :textures :texture_cache
 
   class << self
     attr_accessor :instance
