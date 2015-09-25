e616cb4bbca83a0d4163c77822ea0adb73a1e5b9
Corey Powell <mistdragon100@gmail.com>
2015-09-25 01:16:19 -0500

    Updated TextInput controller
    :insert is now :replace
    modes are validated, fixed backspace and delete index changes

--- scripts/controllers/text_input.rb
+++ scripts/controllers/text_input.rb
@@ -2,8 +2,8 @@ module Controllers
   # Helper controller for handling text input, this class will handle typing,
   # insert and appending.
   # In order to use this class, simply pass in an Object that responds to
-  # #string and #string=, such as a Moon::Text or Moon::Label.
-  # :insert mode will replace characters on the index, while :append will
+  # `#string` and `#string=`, such as a {Moon::Text} or {Moon::Label}.
+  # `:replace` mode will replace characters on the index, while `:append` will
   # add new characters, possibly injecting into the string
   class TextInput
     # @return [#string, #string=]
@@ -12,8 +12,6 @@ module Controllers
     attr_reader :input
     # @return [Integer]
     attr_accessor :index
-    # @return [Symbol]
-    attr_accessor :mode
 
     # @param [#string, #string=] target
     def initialize(target, options = {})
@@ -35,8 +33,8 @@ module Controllers
       when :backspace then backspace
       when :delete    then delete
       when :insert    then toggle_input_mode
-      when :left  then cursor_prev
-      when :right then cursor_next
+      when :left      then cursor_prev
+      when :right     then cursor_next
       end
     end
 
@@ -45,61 +43,96 @@ module Controllers
       @index = inx.clamp(0, @target.string.size)
     end
 
-    # Toggles the mode, if insert switches to append, if append switches to
-    # insert
-    def toggle_input_mode
-      @mode = @mode == :insert ? :append : :insert
+    # @return [Symbol]
+    attr_reader :mode
+
+    # @param [Symbol] mode
+    def mode=(mode)
+      if mode == :append || mode == :replace
+        @mode = mode
+      else
+        raise ArgumentError,
+          "invalid mode #{mode} (expected :append or :replace)"
+      end
     end
 
-    # move's the cursor back
+    # Toggles the mode, if `:replace` switches to `:append`,
+    # if `:append` switches to `:replace`
+    #
+    # @return [self]
+    def toggle_input_mode
+      self.mode = @mode == :replace ? :append : :replace
+      self
+    end
+
+    # Move's the cursor back
+    #
+    # @return [self]
     def cursor_prev
       self.index = @index.pred
       @target.string = @target.string
+      self
     end
 
-    # move's the cursor forward
+    # Move's the cursor forward
+    #
+    # @return [self]
     def cursor_next
       self.index = @index.succ
       @target.string = @target.string
+      self
     end
 
+    # @return [self]
     private def modify
       src = @target.string
       return if src.empty?
       return if @index >= src.size
       src = yield src
-      @index = @index.pred.clamp(0, src.size)
       @target.string = src
+      self
     end
 
     # Deletes the character to the right of the cursor
+    #
+    # @return [self]
     def delete
       modify do |src|
-        (src.slice(0, @index) || '') <<
+        src = (src.slice(0, @index) || '') <<
         (src.slice((@index + 1)..src.size) || '')
+        @index = @index.clamp(0, src.size)
+        src
       end
     end
 
     # Deletes the character to the left of the cursor
+    #
+    # @return [self]
     def backspace
       modify do |src|
-        (src.slice(0...(@index - 1)) || '') <<
+        src = (src.slice(0...(@index - 1)) || '') <<
         (src.slice(@index..src.size) || '')
+        @index = @index.pred.clamp(0, src.size)
+        src
       end
     end
 
+    # Inserts the given string +str+ into the {#target}
+    #
     # @param [String] str
+    # @return [self]
     def insert(str)
       src = @target.string
       case @mode
       when :append
         src = (src.slice(0, @index) || '') << str <<
           (src.slice(@index..src.size) || '')
-      when :insert
-        src[@index] = str
+      when :replace
+        src[@index, str.size] = str
       end
       @index = (@index + str.size).clamp(0, src.size)
       @target.string = src
+      self
     end
   end
 end
--- spec/scripts/controllers/text_input_spec.rb
+++ spec/scripts/controllers/text_input_spec.rb
@@ -14,8 +14,96 @@ end
 describe Controllers::TextInput do
   context '#initialize' do
     it 'initializes a TextInput controller' do
-      text = TextObj.new
+      text = Fixtures::TextObj.new
       text_input = described_class.new(text)
+    end
+  end
+
+  context 'string manipulation' do
+    let(:text_input) do
+      @text_input ||= begin
+        text = Fixtures::TextObj.new
+        described_class.new(text)
+      end
+    end
+
+    context '#insert' do
+      context 'append mode' do
+        it 'inserts a string' do
+          text_input.mode = :append
+          text_input.insert('abc')
+          expect(text_input.target.string).to eq('abc')
+        end
+
+        it 'will append a string if the index is reset' do
+          text_input.target.string = 'abc'
+          text_input.mode = :append
+          text_input.index = 0
+          text_input.insert('_def')
+          expect(text_input.target.string).to eq('_defabc')
+        end
+
+        it 'will insert a string if the index is inside the string' do
+          text_input.target.string = '_defabc'
+          text_input.mode = :append
+          text_input.index = 4
+          text_input.insert('-0_0-')
+          expect(text_input.target.string).to eq('_def-0_0-abc')
+        end
+      end
+
+      context 'replace mode' do
+        it 'replaces the contents of the string' do
+          text_input.mode = :replace
+
+          text_input.target.string = '_def-0_0-abc'
+          text_input.index = 0
+          text_input.insert('defa')
+          expect(text_input.target.string).to eq('defa-0_0-abc')
+
+          text_input.index = 4
+          text_input.insert('1234')
+          expect(text_input.target.string).to eq('defa1234-abc')
+
+          # works like append if at the end of the string
+          text_input.index = text_input.target.string.size
+          text_input.insert('-end')
+          expect(text_input.target.string).to eq('defa1234-abc-end')
+        end
+      end
+    end
+
+    context '#backspace' do
+      it 'erases a character left of the cursor' do
+        text_input.target.string = 'abdc'
+        text_input.index = 3
+        text_input.backspace
+        expect(text_input.target.string).to eq('abc')
+      end
+    end
+
+    context '#delete' do
+      it 'erases a character right of the cursor' do
+        text_input.target.string = 'abdc'
+        text_input.index = 2
+        text_input.delete
+        expect(text_input.target.string).to eq('abc')
+      end
+    end
+
+    context '#cursor_(prev|next)' do
+      it 'moves the index' do
+        text_input.target.string = 'abcd'
+        text_input.index = 0
+
+        # should be clamped
+        text_input.cursor_prev
+        expect(text_input.index).to eq(0)
+
+
+        text_input.cursor_next
+        expect(text_input.index).to eq(1)
+      end
     end
   end
 end
