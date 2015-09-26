091cec60c82182d98c200c26c1f97e5a67262192
Corey Powell <mistdragon100@gmail.com>
2015-09-26 00:38:11 -0500

    Updated TileData model

--- scripts/models/tile_data.rb
+++ scripts/models/tile_data.rb
@@ -4,9 +4,11 @@ require 'scripts/models/map'
 module Models
   class TileData < Base
     field :valid,    type: Boolean,       default: false
-    field :passage,  type: Integer,       default: 0
-    array :tile_ids, type: Integer
     field :map,      type: Map,           allow_nil: true, default: nil
     field :position, type: Moon::Vector3, default: Moon::Vector3.zero
+    field :passage,  type: Integer,       default: 0
+    array :tile_ids, type: Integer
+    array :zone_ids, type: Integer
+    array :nodes,    type: Hash
   end
 end
